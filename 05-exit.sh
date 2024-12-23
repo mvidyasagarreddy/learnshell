82ee5e2c886959ea1247e15c6f9fedde057adb78
Mukkara Vidya Sagar Reddy <mvsreddy2018@gmail.com>
2024-12-23 07:36:11 +0530

    Roboshop shell

--- 05-exit.sh
+++ 05-exit.sh
@@ -1,5 +1,6 @@
 echo hello
 #we can give exit status from 1 -- 255
 exit 1
-#after printing hello , then exit $? will give result of 1
+#after printing hello , then exit $? will give result of 1. If you give exit 100
+#it will give output of echo$? as 100
 echo bye
