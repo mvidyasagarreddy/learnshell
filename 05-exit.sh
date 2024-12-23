959856d171afa5c0a16c003ac4806723046e3c63
Mukkara Vidya Sagar Reddy <mvsreddy2018@gmail.com>
2024-12-23 07:54:28 +0530

    Roboshop shell

--- 05-exit.sh
+++ 05-exit.sh
@@ -4,3 +4,14 @@ exit 1
 #after printing hello , then exit $? will give result of 1. If you give exit 100
 #it will give output of echo$? as 100
 echo bye
+
+#This exit status concept can also be implemented in functions
+#But in functions we use return to come out of the functions
+sample(){
+  echo hello
+  return 1
+  echo bye
+}
+
+sample
+echo sample function exit status - $?
